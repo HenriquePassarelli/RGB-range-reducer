f30eb6cc670cde124357835cebba4895b925cc51
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-10-08 17:32:16 -0300

    removing ideas

--- color.js
+++ color.js
@@ -1,27 +1,4 @@
-// hsv - hsl
-/* 
-    range - hsl(hue, saturation, lightness)
-    -> Hue is a degree on the color wheel from 0 to 360. 0 is red, 120 is green, 240 is blue.
-    -> Saturation is a percentage value; 0% means a shade of gray and 100% is the full color.
-    -> Lightness is also a percentage; 0% is black, 100% is white.
-    h- signal 
-    red - 0 to 20
-    orange - 20 to 60
-    yellow - 60 to 80 
-    green - 80 to 180
-    blue - 180 to 260
-    purple - 260 to 295
-    pink - 295 to 347
-    red - 347
 
-    s-  signal 
-    s - 0 to 10 = gray
-
-
-    l - signal
-    l- 100 = white
-    l- 0 to 10 = black
-*/
 let array = {
     color_dominant_2_rgb: [
         0,
