5edcc3dce17d7d6c524fc51b7d7ad779967637ae
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-10-08 17:14:47 -0300

    first idea

---
+++ README.md
@@ -0,0 +1,27 @@
+ 
+    ### range - hsl(hue, saturation, lightness)
+
+    -> Hue is a degree on the color wheel from 0 to 360. 0 is red, 120 is green, 240 is blue.
+
+    -> Saturation is a percentage value; 0% means a shade of gray and 100% is the full color.
+
+    -> Lightness is also a percentage; 0% is black, 100% is white.
+
+<https://i2.wp.com/css-tricks.com/wp-content/uploads/2019/01/rgb_color_wheel_60.png?ssl=1>
+
+   > ###### h- signal 
+   > red - 0 to 20
+   > orange - 20 to 60
+   > yellow - 60 to 80 
+   > green - 80 to 180
+   > blue - 180 to 260
+   > purple - 260 to 295
+   > pink - 295 to 347
+   > red - 347
+ 
+   > ###### s -  signal 
+   > s - 0 to 10 = gray
+ 
+   > ###### l - signal
+   > l- 100 = white
+   > l- 0 to 10 = black
---
+++ color.js
@@ -0,0 +1,139 @@
+// hsv - hsl
+/* 
+    range - hsl(hue, saturation, lightness)
+    -> Hue is a degree on the color wheel from 0 to 360. 0 is red, 120 is green, 240 is blue.
+    -> Saturation is a percentage value; 0% means a shade of gray and 100% is the full color.
+    -> Lightness is also a percentage; 0% is black, 100% is white.
+    h- signal 
+    red - 0 to 20
+    orange - 20 to 60
+    yellow - 60 to 80 
+    green - 80 to 180
+    blue - 180 to 260
+    purple - 260 to 295
+    pink - 295 to 347
+    red - 347
+
+    s-  signal 
+    s - 0 to 10 = gray
+
+
+    l - signal
+    l- 100 = white
+    l- 0 to 10 = black
+*/
+let array = {
+    color_dominant_2_rgb: [
+        0,
+        400,
+        255
+    ],
+}
+let myColor = array.color_dominant_2_rgb;
+RGBToHSL(myColor[0], myColor[1], myColor[2])
+
+function RGBToHSL(r, g, b) {
+
+    // treat high values 
+    if (r > 255) r = 255;
+    if (g > 255) g = 255;
+    if (b > 255) b = 255;
+
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+    // Calculate hue
+    // No difference
+    if (delta == 0)
+        h = 0;
+    // Red is max
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    // Blue is max
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    console.log("hsl(" + h + "," + s + "%," + l + "%)")
+
+    return color(h, s, l);
+}
+
+function color(h, s, l) {
+
+    console.log(h + "," + s + "," + l)
+
+    console.log(!(98 <= l))
+
+    switch (true) {
+        case 0 <= h && h <= 20 || 345 <= h && h <= 360:
+            if (0 <= s && s <= 10 || 0 <= l && l <= 10 || 98 <= l) {
+                if (0 <= s && s <= 10 && !(98 <= l) && !(0 <= l && l <= 10)) {
+                    console.log("Gray")
+                }
+                else if (0 <= l && l <= 10) {
+                    console.log("Black")
+                } else if (98 <= l) {
+                    console.log("White")
+                }
+                break;
+            } else {
+                console.log("Red")
+                break;
+            }
+        case 20 < h && h <= 60:
+            console.log("Orange")
+            break;
+        case 60 < h && h <= 80:
+            console.log("Yellow")
+            break;
+        case 80 < h && h <= 180:
+            console.log("Green")
+            break;
+        case 180 < h && h <= 260:
+            console.log("Blue")
+            break;
+        case 260 < h && h <= 295:
+            console.log("purple")
+            break;
+        case 295 < h && h <= 347:
+            console.log("Pink ")
+            break;
+        case 0 <= s && s >= 10:
+
+        default:
+            console.log("Color not found")
+            break;
+    }
+
+
+}
+
