088e32e2149c4abc7801997933a6361751956eb2
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-10-10 21:18:40 -0300

    solving red and pink issues and adding some test

--- color.js
+++ color.js
@@ -59,66 +59,97 @@ function RGBToHSL(r, g, b) {
     s = +(s * 100).toFixed(1);
     l = +(l * 100).toFixed(1);
 
-    console.log("hsl(" + h + "," + s + "%," + l + "%)")
+    //console.log("hsl(" + h + "," + s + "%," + l + "%)")
 
     return color(h, s, l);
 }
 
 function color(h, s, l) {
 
-    console.log(h + "," + s + "," + l)
-
-    console.log(s >= 26 && 15 <= l && l <= 30)
-
     switch (true) {
-        case 0 <= h && h <= 15 || 350 < h && h <= 360:
+        case 0 <= h && h <= 15 || 290 < h && h <= 360:
             if (0 <= s || 0 <= l && l <= 10 || 98 <= l) {
                 if (s >= 26 && 15 <= l && l <= 30) {
-                    console.log("Brown")
-
+                    return "Brown"
                 } else if (0 <= s && s <= 10 && !(98 <= l) && !(0 <= l && l <= 10)) {
-                    console.log("Gray")
+                    return "Gray"
                 }
                 else if (0 <= l && l <= 10) {
-                    console.log("Black")
+                    return "Black"
                 } else if (98 <= l) {
-                    console.log("White")
-                } else console.log("Red")
-                break;
-            } else {
-                console.log("3")
-                break;
+                    return "White"
+                } else if (51 <= l) {
+                    return "Pink"
+                } else return "Red"
             }
         case 15 < h && h <= 46:
             if (s >= 26 && 10 <= l && l <= 40) {
-                console.log("Brown")
-            } else console.log("Orange")
-            break;
+                return "Brown"
+            } else return "Orange"
         case 46 < h && h <= 70:
             //brown 20 to 50
             if (s >= 26 && 10 <= l && l <= 30) {
-                console.log("Brown")
-            } else console.log("Yellow")
-            break;
+                return "Brown"
+            } else return "Yellow"
         case 70 < h && h <= 182:
-            console.log("Green")
-            break;
+            return "Green"
         case 182 < h && h <= 240:
-            console.log("Blue")
-            break;
+            return "Blue"
         case 240 < h && h <= 290:
-            console.log("purple")
-            break;
-        case 290 < h && h <= 350:
-            console.log("Pink ")
-            break;
+            return "Purple"
+        /* case 290 < h && h <= 359:
+            return "Pink " */
         default:
-            console.log("Color not found")
-            break;
+            return "Color not found"
     }
-
-
 }
 
-// brown 10 <= l && l <=30 && s >= 26
-//pink || l > = 60
+// test
+
+
+console.log("expected - black /", "result - ", RGBToHSL(0, 0, 0))
+console.log("expected - white /", "result - ", RGBToHSL(255, 255, 255))
+console.log("expected - brown /", "result - ", RGBToHSL(180, 89, 24))
+console.log("expected - brown /", "result - ", RGBToHSL(92, 29, 0))
+console.log("expected - brown /", "result - ", RGBToHSL(128, 0, 0))
+console.log("expected - brown /", "result - ", RGBToHSL(103, 72, 34))
+console.log("expected - orange /", "result - ", RGBToHSL(255, 69, 0))
+console.log("expected - orange /", "result - ", RGBToHSL(255, 94, 0))
+console.log("expected - gray /", "result - ", RGBToHSL(77, 77, 77))
+console.log("expected - gray /", "result - ", RGBToHSL(192, 192, 192))
+console.log("expected - gray /", "result - ", RGBToHSL(220, 220, 220))
+console.log("expected - gray /", "result - ", RGBToHSL(105, 105, 105))
+console.log("expected - yellow /", "result - ", RGBToHSL(255, 255, 0))
+console.log("expected - yellow /", "result - ", RGBToHSL(255, 215, 0))
+console.log("expected - yellow /", "result - ", RGBToHSL(240, 230, 140))
+console.log("expected - red /", "result - ", RGBToHSL(245, 0, 0))
+console.log("expected - red /", "result - ", RGBToHSL(255, 0, 0))
+console.log("expected - red /", "result - ", RGBToHSL(193, 21, 47))
+console.log("expected - red /", "result - ", RGBToHSL(178, 34, 34))
+console.log("expected - pink /", "result - ", RGBToHSL(255, 105, 180))
+console.log("expected - pink /", "result - ", RGBToHSL(255, 192, 203))
+console.log("expected - pink /", "result - ", RGBToHSL(230, 192, 203))
+console.log("expected - pink /", "result - ", RGBToHSL(234, 164, 164))
+console.log("expected - pink /", "result - ", RGBToHSL(255, 133, 133))
+console.log("expected - pink /", "result - ", RGBToHSL(248, 223, 216))
+console.log("expected - purple /", "result - ", RGBToHSL(246, 230, 250))
+console.log("expected - purple /", "result - ", RGBToHSL(230, 223, 250))
+console.log("expected - purple /", "result - ", RGBToHSL(86, 40, 129))
+console.log("expected - blue /", "result - ", RGBToHSL(176, 196, 222))
+console.log("expected - blue /", "result - ", RGBToHSL(0, 0, 128))
+console.log("expected - blue /", "result - ", RGBToHSL(25, 25, 112))
+console.log("expected - blue /", "result - ", RGBToHSL(30, 144, 255))
+console.log("expected - green /", "result - ", RGBToHSL(127, 255, 212))
+console.log("expected - green /", "result - ", RGBToHSL(95, 158, 160))
+console.log("expected - green /", "result - ", RGBToHSL(46, 139, 87))
+console.log("expected - green /", "result - ", RGBToHSL(0, 255, 0))
+console.log("expected - green /", "result - ", RGBToHSL(0, 100, 0))
+console.log("expected - green /", "result - ", RGBToHSL(154, 205, 50))
+
+
+
+
+
+
+
+
